da609b38c3efaa9e9c5ad1ac0c35284b91c2ac13
Fer <b1tdreamer@tabahack.tk>
2014-06-26 22:39:46 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<?
+//php code
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+    <title>¿Qué quieres hacer?</title>
+    //Latest Bootstrap
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+    <header>
+      <img title="logo" src="img/logo.png" />
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Exit</li>
+        </ul>
+      </nav>
+    </header>
+    <divt>
+      <ul>
+        <li><span>Aprender</span></li>
+        <li><span>Informarte</span></li>
+        <li><span>Entretenerte</span></li>
+        <li><span>Construir</span></li>
+        <li><span>Reflexionar</span></li>
+      </ul>
+    </div>
+    <footer><span>Compartir es vivir</span></footer>
+  </body>
+</html>
